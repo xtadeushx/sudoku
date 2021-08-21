07f238455a1d7f6422fd020e23b1c658ad508139
Oleksandr Cherkasov <alekscherkasov13@gmail.com>
2021-08-21 11:39:30 +0300

    fill project js-clock

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+module.exports = function solveSudoku(board) {
+
+  const size = 9;
+  const boxSize = 3;
+
+  const findEmpty = (board) => {
+    for (let r = 0; r < size; r++) {
+      for (let c = 0; c < size; c++) {
+        if (board[r][c] === 0) {
+          return [r, c];
+        }
+      }
+    }
+    return null;
+  }
+
+  const validate = (num, pos, board) => {
+    const [r, c] = pos;
+
+    //Check rows
+    for (let i = 0; i < size; i++) {
+      if (board[i][c] === num && i !== r) {
+        return false;
+      }
+    }
+
+    //Check cols
+    for (let i = 0; i < size; i++) {
+      if (board[r][i] === num && i !== c) {
+        return false;
+      }
+    }
+
+
+    //Check box
+    const boxRow = Math.floor(r / boxSize) * boxSize;
+    const boxCol = Math.floor(c / boxSize) * boxSize;
+
+    for (let i = boxRow; i < boxRow + boxSize; i++) {
+      for (let j = boxCol; j < boxCol + boxSize; j++) {
+        if (board[i][j] === num && i !== r && j !== c) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  const solve = () => {
+    const currPos = findEmpty(board);
+
+    if (currPos === null) {
+      return true;
+    }
+    //console.log('------------------------------');
+    for (let i = 1; i < size + 1; i++) {
+      const currNum = i;
+      const isValid = validate(currNum, currPos, board);
+      //console.log('currPos ', currPos, 'currNum ',currNum, 'isValid ',isValid);
+      if (isValid) {
+        const [x, y] = currPos;
+        board[x][y] = currNum;
+
+        if (solve()) {
+          return true;
+        }
+
+        board[x][y] = 0;
+      }
+    }
+
+    return false;
+  }
+
+  solve();
+  return board;
 }
+
